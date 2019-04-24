ffdf669dd80da07f12f55247becf25add487b40d
Taha Amjad <tamjd1@gmail.com>
2019-04-24 00:01:58 -0400

    docstring and refactoring

--- compare.py
+++ compare.py
@@ -9,54 +9,64 @@ from sklearn.naive_bayes import GaussianNB
 
 from timer import Timer
 
-X_train = None
-X_test = None
-y_train = None
-y_test = None
 
+def _prepare_data(data_file="./data/data_banknote_authentication.txt", training_size=750):
+    """
+    Read in data from input file and parse it into features and class labels arrays
+    Use sklearn's train_test_split function to split the data into training and testing sets
 
-def prepare():
-    with open("./data/data_banknote_authentication.txt") as f:
+    :param data_file: path to data file (str, default: "./data/data_banknote_authentication.txt"
+    :param training_size: count or percentage of training data points (int, float, default: 750)
+    :return:
+        X_train : training features (numpy.ndarray)
+        X_test : testing features (numpy.ndarray)
+        y_train : training class labels (list)
+        y_test : testing class labels (list)
+    """
+    with open(data_file) as f:
         reader = csv.reader(f)
         rows = [row for row in reader]
-        global X_train, X_test, y_train, y_test
         X = np.array([[np.float(item) for item in row[:-1]] for row in rows])
         y = [row[-1] for row in rows]
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=750)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=training_size)
+
+    return X_train, X_test, y_train, y_test
 
 
-def _determine_optimal_gamma_value(k=3):
+def _determine_optimal_gamma_value(X, y, k=3):
     """
     SVM 3-fold Cross Validation to determine optimal gamma value
-    Note: gamma value in this function is the inverse of the
-    standard deviation of the Radial Basis Function kernel
-    used as a similarity measure between two points
-    :return: gamma value
+
+    Note: gamma value in sklearn.svm is the inverse of the
+        standard deviation of the Radial Basis Function kernel
+        used as a similarity measure between two points
+    :param X: features (numpy.ndarray)
+    :param y: labels (list)
+    :param k: number of folds (int, default: 3)
+    :return:
     """
     # possible values of gamma to test with
-    test_gamma_values = [0.01, 1/X_train.shape[1], 1.0]
+    test_gamma_values = [0.01, 1/X.shape[1], 1.0]
 
     # dictionary of gamma value index and scores for determining
     # optimal gamma value based on cross validation tests
     gamma_value_scores = {0: [], 1: [], 2: []}
 
-    X = X_train
-    y = y_train
-
     # 3 folds, repeated 3 times; once for each value of gamma
     rkf = RepeatedKFold(n_splits=k, n_repeats=len(test_gamma_values))
     for i, (train_indices, test_indices) in enumerate(rkf.split(X, y)):
-        _X_train = np.array([X[_i] for _i in train_indices])
-        _y_train = [y[_i] for _i in train_indices]
-        _X_test = np.array([X[_i] for _i in test_indices])
-        _y_test = [y[_i] for _i in test_indices]
+        X_train = np.array([X[_i] for _i in train_indices])
+        y_train = [y[_i] for _i in train_indices]
+        X_test = np.array([X[_i] for _i in test_indices])
+        y_test = [y[_i] for _i in test_indices]
 
         gamma_value_index = i // len(test_gamma_values)
         gamma = test_gamma_values[gamma_value_index]
         svc = SVC(kernel="rbf", gamma=gamma)
-        svc.fit(X_train, y_train)
-        score = svc.score(X_train, y_train)
+        svc.fit(X_train, y_train)  # train
+        y_pred = svc.predict(X_test)  # test
+        score = accuracy_score(y_test, y_pred)  # score
         gamma_value_scores[gamma_value_index].append(score)
 
     max_score = 0
@@ -78,7 +88,18 @@ def _determine_optimal_gamma_value(k=3):
     return best_gamma_value
 
 
-def classify():
+def main():
+    """
+    Entry point for comparing classifiers
+
+    Compares training time, prediction accuracy, confusion matrices
+        of the following classifiers:
+    1. Logistic Regression
+    2. Support Vector Machine
+    3. Adaboost with Naive Bayes base estimator
+    """
+    X_train, X_test, y_train, y_test = _prepare_data()
+
     print("\n--- Logistic Regression Classifier ---")
     with Timer() as t:
         logistic_regression = LogisticRegression()
@@ -98,7 +119,7 @@ def classify():
     # ------------------------------------------------------------------------------------------------------------------
 
     print("\n--- Support Vector Machine Classifier ---")
-    gamma = _determine_optimal_gamma_value()
+    gamma = _determine_optimal_gamma_value(X_train, y_train)
     with Timer() as t:
         # use Radial Basis Function kernel and gamma value
         # determined by 3-fold cross validation checks
@@ -139,5 +160,4 @@ def classify():
 
 
 if __name__ == '__main__':
-    prepare()
-    classify()
+    main()
